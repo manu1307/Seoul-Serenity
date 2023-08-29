8835a4eb8f8ff9b8145897434743892973a8f8d5
manu1307 <jasonsc@naver.com>
2023-08-29 11:02:15 +0900

    Chore : 헤더, 푸터 컴포넌트 파일 생성 및 분리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
+import Footer from "@/components/Layout/Footer";
+import Header from "@/components/Layout/Header";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
-      <header>Header</header>
+      <Header />
       <div>
         <div>Category</div>
         <div>날씨</div>
         <div>가까운 곳 3개 사진</div>
       </div>
-      <footer>footer</footer>
+      <Footer />
     </main>
   );
 }
