caf2537b1efd8e1c5214562b39b109f49489b690
manu1307 <jasonsc@naver.com>
2023-08-29 23:38:12 +0900

    Chore : 카테고리 배경 이미지 파일 추가, Card 컴포넌트 파일 생성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="p-[20px]">{children}</body>
     </html>
   );
 }
--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Header() {
-  return <header>Header</header>;
+  return (
+    <header className="h-[80px] py-[20px] text-2xl flex items-end gap-2">
+      <span className="text-xl opacity-50">Welcome to </span>
+      Seoul Serenity
+    </header>
+  );
 }
 
 export default Header;
