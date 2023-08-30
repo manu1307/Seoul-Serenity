98328b500ad55b233c0da42f11f0fc2a70ca4afe
manu1307 <jasonsc@naver.com>
2023-08-30 09:57:23 +0900

    Feat : 홈페이지  카테고리 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import CategoryCard from "@/components/Card/CategoryCard";
 import Footer from "@/components/Layout/Footer";
 import Header from "@/components/Layout/Header";
 import Image from "next/image";
@@ -7,7 +8,26 @@ export default function Home() {
     <main>
       <Header />
       <div>
-        <div>Category</div>
+        <div className="flex overflow-x-scroll gap-4 scrollbar-hide">
+          <CategoryCard
+            categoryName="미술관"
+            coverImageUrl="/images/art-museum.png"
+            alt="art-museum"
+            link="/"
+          />
+          <CategoryCard
+            categoryName="공원"
+            coverImageUrl="/images/park.png"
+            alt="art-museum"
+            link="/"
+          />
+          <CategoryCard
+            categoryName="둘레길"
+            coverImageUrl="/images/walking-road.png"
+            alt="art-museum"
+            link="/"
+          />
+        </div>
         <div>날씨</div>
         <div>가까운 곳 3개 사진</div>
       </div>
--- src/components/Card/CategoryCard.tsx
+++ src/components/Card/CategoryCard.tsx
@@ -1,7 +1,27 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
-function CategoryCard() {
-  return <div>CategoryCard</div>;
+type CategoryCardProps = {
+  categoryName: string;
+  coverImageUrl: string;
+  alt: string;
+  link: string;
+};
+
+function CategoryCard({ categoryName, coverImageUrl, alt, link }: CategoryCardProps) {
+  return (
+    <div className="relative w-fit">
+      <Link href={link}>
+        <div className="absolute bottom-2 right-2 z-10 opacity-100 text-xl font-semibold text-white">
+          {categoryName}
+        </div>
+        <div className="relative w-[150px] h-[110px] rounded-2xl categoryCard">
+          <Image className="rounded-2xl opacity-60 " src={coverImageUrl} fill={true} alt={alt} />
+        </div>
+      </Link>
+    </div>
+  );
 }
 
 export default CategoryCard;
